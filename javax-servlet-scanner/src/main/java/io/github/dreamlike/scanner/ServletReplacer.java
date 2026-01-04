e92a6d19cdc8c1875b0ca0e7a9ca16c5da7c80e9
dreamlike-ocean <dreamlike.vertx@gmail.com>
2026-01-04 22:37:41 +0800

    refactor(scanner): 重构ServletReplacer类的字节码转换逻辑
    - 实现了完整的CodeModel转换功能，包括指令、异常处理和注解的处理
    - 添加了对各种指令类型的转换支持，如常量加载、动态调用、方法调用等
    - 实现了常量池项的转换逻辑，包括类引用、字符串、方法类型等
    - 优化了UTF-8字符串的转换逻辑，同时处理二进制名和类名替换
    - 简化了方法元素处理流程，提高代码可读性和维护性

--- javax-servlet-scanner/src/main/java/io/github/dreamlike/scanner/ServletReplacer.java
+++ javax-servlet-scanner/src/main/java/io/github/dreamlike/scanner/ServletReplacer.java
@@ -2,12 +2,10 @@ package io.github.dreamlike.scanner;
 
 import java.lang.classfile.*;
 import java.lang.classfile.attribute.*;
-import java.lang.classfile.constantpool.ClassEntry;
-import java.lang.classfile.constantpool.ConstantPoolBuilder;
-import java.lang.classfile.constantpool.StringEntry;
-import java.lang.classfile.constantpool.Utf8Entry;
+import java.lang.classfile.constantpool.*;
+import java.lang.classfile.instruction.*;
+import java.lang.constant.MethodTypeDesc;
 import java.util.List;
-import java.util.stream.Collectors;
 
 class ServletReplacer implements ClassTransform {
     private static final String SOURCE_BINARY_NAME = "javax/servlet";
@@ -50,6 +48,10 @@ class ServletReplacer implements ClassTransform {
                 methodModel.flags().flagsMask(),
                 builder -> {
                     for (MethodElement element : methodModel.elementList()) {
+                        if (element instanceof CodeModel codeModel) {
+                            transformCodeModel(builder, codeModel);
+                            continue;
+                        }
                         MethodElement methodElement = switch (element) {
                             case AnnotationDefaultAttribute annotationDefaultAttribute ->
                                     AnnotationDefaultAttribute.of(transformAnnotationValue(builder, annotationDefaultAttribute.defaultValue()));
@@ -74,11 +76,7 @@ class ServletReplacer implements ClassTransform {
                                     SignatureAttribute.of(transformUtf8Entry(builder, signatureAttribute.signature()));
                             default -> element;
                         };
-                        if (methodElement instanceof CodeModel codeModel) {
-                            transformCodeModel(builder, codeModel);
-                        } else {
-                            builder.with(methodElement);
-                        }
+                        builder.with(methodElement);
                     }
                 }
         );
@@ -86,10 +84,181 @@ class ServletReplacer implements ClassTransform {
 
 
     private void transformCodeModel(MethodBuilder methodBuilder, CodeModel codeModel) {
-        //todo...
-        for (CodeElement codeElement : codeModel.elementList()) {
+        methodBuilder.withCode(codeBuilder -> {
+            for (CodeElement codeElement : codeModel.elementList()) {
+                switch (codeElement) {
+                    case Instruction instruction -> transformInstruction(codeBuilder, instruction);
+                    case ExceptionCatch exceptionCatch -> codeBuilder.with(ExceptionCatch.of(
+                            exceptionCatch.tryStart(),
+                            exceptionCatch.tryEnd(),
+                            exceptionCatch.handler(),
+                            exceptionCatch.catchType().map(c -> transformClassEntry(codeBuilder, c))
+                    ));
+                    case RuntimeVisibleTypeAnnotationsAttribute runtimeVisibleTypeAnnotationsAttribute -> codeBuilder.with(RuntimeVisibleTypeAnnotationsAttribute.of(
+                            runtimeVisibleTypeAnnotationsAttribute.annotations().stream().map(a -> transformTypeAnnotation(codeBuilder, a)).toList()
+                    ));
 
+                    default -> codeBuilder.with(codeElement);
+                }
+            }
+        });
+    }
+
+    private void transformInstruction(CodeBuilder codeBuilder, Instruction instruction) {
+        Instruction afterProcess = switch (instruction) {
+            case ConstantInstruction.LoadConstantInstruction loadConstantInstruction -> {
+                LoadableConstantEntry loadableConstantEntry = transformLoadableConstantEntry(codeBuilder, loadConstantInstruction.constantEntry());
+                if (loadableConstantEntry == loadConstantInstruction.constantEntry()) {
+                    yield instruction;
+                }
+                yield ConstantInstruction.ofLoad(loadConstantInstruction.opcode(), loadableConstantEntry);
+            }
+            case InvokeDynamicInstruction invokeDynamicInstruction -> {
+                InvokeDynamicEntry invokeDynamicEntry = transformInvokeDynamicEntry(codeBuilder, invokeDynamicInstruction.invokedynamic());
+                if (invokeDynamicEntry == invokeDynamicInstruction.invokedynamic()) {
+                    yield instruction;
+                }
+                yield InvokeDynamicInstruction.of(invokeDynamicEntry);
+            }
+            case InvokeInstruction invokeInstruction -> {
+                MemberRefEntry memberRefEntry = transformMemberRefEntry(codeBuilder, invokeInstruction.method());
+                if (memberRefEntry == invokeInstruction.method()) {
+                    yield instruction;
+                }
+                yield InvokeInstruction.of(invokeInstruction.opcode(), memberRefEntry);
+            }
+            case FieldInstruction fieldInstruction -> {
+                FieldRefEntry fieldRefEntry = transformFieldRefEntry(codeBuilder, fieldInstruction.field());
+                if (fieldRefEntry == fieldInstruction.field()) {
+                    yield instruction;
+                }
+                yield FieldInstruction.of(fieldInstruction.opcode(), fieldRefEntry);
+            }
+            case NewObjectInstruction newObjectInstruction -> {
+                ClassEntry classEntry = transformClassEntry(codeBuilder, newObjectInstruction.className());
+                if (classEntry == newObjectInstruction.className()) {
+                    yield instruction;
+                }
+                yield NewObjectInstruction.of(classEntry);
+            }
+            case NewReferenceArrayInstruction newReferenceArrayInstruction -> {
+                ClassEntry classEntry = transformClassEntry(codeBuilder, newReferenceArrayInstruction.componentType());
+                if (classEntry == newReferenceArrayInstruction.componentType()) {
+                    yield instruction;
+                }
+                yield NewReferenceArrayInstruction.of(classEntry);
+            }
+            case TypeCheckInstruction typeCheckInstruction -> {
+                ClassEntry classEntry = transformClassEntry(codeBuilder, typeCheckInstruction.type());
+                if (classEntry == typeCheckInstruction.type()) {
+                    yield instruction;
+                }
+                yield TypeCheckInstruction.of(typeCheckInstruction.opcode(), classEntry);
+            }
+            default -> instruction;
+        };
+
+        codeBuilder.with(afterProcess);
+    }
+
+    private InvokeDynamicEntry transformInvokeDynamicEntry(ClassFileBuilder builder, InvokeDynamicEntry invokeDynamicEntry) {
+        BootstrapMethodEntry bootstrapMethodEntry = transformBootstrapMethodEntry(builder, invokeDynamicEntry.bootstrap());
+        NameAndTypeEntry nameAndTypeEntry = transformNameAndTypeEntry(builder, invokeDynamicEntry.nameAndType());
+        if (bootstrapMethodEntry == invokeDynamicEntry.bootstrap() && nameAndTypeEntry == invokeDynamicEntry.nameAndType()) {
+            return invokeDynamicEntry;
         }
+        return builder.constantPool().invokeDynamicEntry(bootstrapMethodEntry, nameAndTypeEntry);
+    }
+
+    private BootstrapMethodEntry transformBootstrapMethodEntry(ClassFileBuilder builder, BootstrapMethodEntry bootstrapMethodEntry) {
+        MethodHandleEntry methodHandleEntry = transformMethodHandleEntry(builder, bootstrapMethodEntry.bootstrapMethod());
+        List<LoadableConstantEntry> arguments = bootstrapMethodEntry.arguments()
+                .stream()
+                .map(a -> transformLoadableConstantEntry(builder, a))
+                .toList();
+        if (methodHandleEntry == bootstrapMethodEntry.bootstrapMethod() && arguments.equals(bootstrapMethodEntry.arguments())) {
+            return bootstrapMethodEntry;
+        }
+        return builder.constantPool().bsmEntry(methodHandleEntry, arguments);
+    }
+
+    private MethodHandleEntry transformMethodHandleEntry(ClassFileBuilder builder, MethodHandleEntry methodHandleEntry) {
+        MemberRefEntry memberRefEntry = transformMemberRefEntry(builder, methodHandleEntry.reference());
+        if (memberRefEntry == methodHandleEntry.reference()) {
+            return methodHandleEntry;
+        }
+        return builder.constantPool().methodHandleEntry(methodHandleEntry.kind(), memberRefEntry);
+    }
+
+    private MemberRefEntry transformMemberRefEntry(ClassFileBuilder builder, MemberRefEntry memberRefEntry) {
+        ClassEntry owner = transformClassEntry(builder, memberRefEntry.owner());
+        NameAndTypeEntry nameAndTypeEntry = transformNameAndTypeEntry(builder, memberRefEntry.nameAndType());
+        ConstantPoolBuilder constantPool = builder.constantPool();
+        if (memberRefEntry instanceof MethodRefEntry) {
+            return constantPool.methodRefEntry(owner, nameAndTypeEntry);
+        }
+        if (memberRefEntry instanceof InterfaceMethodRefEntry) {
+            return constantPool.interfaceMethodRefEntry(owner, nameAndTypeEntry);
+        }
+        if (memberRefEntry instanceof FieldRefEntry) {
+            return constantPool.fieldRefEntry(owner, nameAndTypeEntry);
+        }
+        return memberRefEntry;
+    }
+
+    private FieldRefEntry transformFieldRefEntry(ClassFileBuilder builder, FieldRefEntry fieldRefEntry) {
+        ClassEntry owner = transformClassEntry(builder, fieldRefEntry.owner());
+        NameAndTypeEntry nameAndTypeEntry = transformNameAndTypeEntry(builder, fieldRefEntry.nameAndType());
+        if (owner == fieldRefEntry.owner() && nameAndTypeEntry == fieldRefEntry.nameAndType()) {
+            return fieldRefEntry;
+        }
+        return builder.constantPool().fieldRefEntry(owner, nameAndTypeEntry);
+    }
+
+    private NameAndTypeEntry transformNameAndTypeEntry(ClassFileBuilder builder, NameAndTypeEntry nameAndTypeEntry) {
+        Utf8Entry type = transformUtf8Entry(builder, nameAndTypeEntry.type());
+        if (type == nameAndTypeEntry.type()) {
+            return nameAndTypeEntry;
+        }
+        return builder.constantPool().nameAndTypeEntry(nameAndTypeEntry.name(), type);
+    }
+
+    private LoadableConstantEntry transformLoadableConstantEntry(ClassFileBuilder builder, LoadableConstantEntry loadableConstantEntry) {
+        return switch (loadableConstantEntry) {
+            case ClassEntry classEntry -> transformClassEntry(builder, classEntry);
+            case StringEntry stringEntry -> {
+                String afterProcess = stringEntry.stringValue()
+                        .replace(SOURCE_BINARY_NAME, TARGET_BINARY_NAME)
+                        .replace(SOURCE_CLASS_NAME, TARGET_CLASS_NAME);
+                if (afterProcess.equals(stringEntry.stringValue())) {
+                    yield stringEntry;
+                }
+                yield builder.constantPool().stringEntry(builder.constantPool().utf8Entry(afterProcess));
+            }
+            case MethodTypeEntry methodTypeEntry -> transformMethodTypeEntry(builder, methodTypeEntry);
+            case MethodHandleEntry methodHandleEntry -> transformMethodHandleEntry(builder, methodHandleEntry);
+            case ConstantDynamicEntry constantDynamicEntry -> transformConstantDynamicEntry(builder, constantDynamicEntry);
+            default -> loadableConstantEntry;
+        };
+    }
+
+    private MethodTypeEntry transformMethodTypeEntry(ClassFileBuilder builder, MethodTypeEntry methodTypeEntry) {
+        MethodTypeDesc methodTypeDesc = methodTypeEntry.asSymbol();
+        String descriptorString = methodTypeDesc.descriptorString();
+        if (!descriptorString.contains(SOURCE_BINARY_NAME)) {
+            return methodTypeEntry;
+        }
+        MethodTypeDesc afterProcess = MethodTypeDesc.ofDescriptor(descriptorString.replace(SOURCE_BINARY_NAME, TARGET_BINARY_NAME));
+        return builder.constantPool().methodTypeEntry(afterProcess);
+    }
+
+    private ConstantDynamicEntry transformConstantDynamicEntry(ClassFileBuilder builder, ConstantDynamicEntry constantDynamicEntry) {
+        BootstrapMethodEntry bootstrapMethodEntry = transformBootstrapMethodEntry(builder, constantDynamicEntry.bootstrap());
+        NameAndTypeEntry nameAndTypeEntry = transformNameAndTypeEntry(builder, constantDynamicEntry.nameAndType());
+        if (bootstrapMethodEntry == constantDynamicEntry.bootstrap() && nameAndTypeEntry == constantDynamicEntry.nameAndType()) {
+            return constantDynamicEntry;
+        }
+        return builder.constantPool().constantDynamicEntry(bootstrapMethodEntry, nameAndTypeEntry);
     }
 
     private void transformFieldModel(ClassBuilder classBuilder, FieldModel fieldModel) {
@@ -180,10 +349,12 @@ class ServletReplacer implements ClassTransform {
     }
 
     private static Utf8Entry transformUtf8Entry(ClassFileBuilder builder, Utf8Entry utf8Entry) {
-        if (!utf8Entry.stringValue().contains(SOURCE_BINARY_NAME)) {
+        if (!utf8Entry.stringValue().contains(SOURCE_BINARY_NAME) && !utf8Entry.stringValue().contains(SOURCE_CLASS_NAME)) {
             return utf8Entry;
         }
-        String afterProcess = utf8Entry.stringValue().replaceAll(SOURCE_BINARY_NAME, TARGET_BINARY_NAME);
+        String afterProcess = utf8Entry.stringValue()
+                .replace(SOURCE_BINARY_NAME, TARGET_BINARY_NAME)
+                .replace(SOURCE_CLASS_NAME, TARGET_CLASS_NAME);
         return builder.constantPool().utf8Entry(afterProcess);
     }
 }
